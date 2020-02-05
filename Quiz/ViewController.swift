cf9970fbff99ba0c45e86368cac228637660cae1
ranudeepreddy <arami6@unh.newhaven.edu>
2020-02-05 13:35:36 -0500

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Ramireddy, Anudeep Reddy on 2/5/20.
+//  Copyright © 2020 Ramireddy, Anudeep Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
