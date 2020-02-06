988e18646e9e4223234ab97f32be1ab2c2ff793d
Anudeep Reddy <usa.anudeep@gmail.com>
2020-02-06 12:07:29 -0500

    I am adding all code for quiz app

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -7,12 +7,51 @@
 //
 
 import UIKit
+import os
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions:[String] = [
+        "What is the Biggest Country?",
+        "What is the Biggest Ocean?",
+        "What is the Longest River?"
+    ]
+    
+    let answers:[String] = [
+        "Russia",
+        "Pacific",
+        "Nile"
+    ]
+    
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_ sender:UIButton){
+        os_log("Showing Next Question")
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count{
+            currentQuestionIndex = 0
+        }
+        
+        let question:String  = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender:UIButton){
+        os_log("Showing Answer")
+        let answer:String  = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        os_log("This is a test log")
+        
+        questionLabel.text = questions[currentQuestionIndex]
+        answerLabel.text = "???"
     }
 
 
